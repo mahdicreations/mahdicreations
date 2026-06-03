18de730c9a577b2a373c21b211bc535eab0e4e46
mahdi <eelmahdi2@alumno.uned.es>
2026-06-03 08:06:47 +0100

    v77

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  // إسكات الخطأ وإعلام Next.js 16 بأنك باغي تخدم بـ Turbopack عادي
-  turbopack: {},
 };
 
 export default nextConfig;
