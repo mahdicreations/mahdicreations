96332bc8eed700db7a90b47286b0fb6213e6414f
mahdi <eelmahdi2@alumno.uned.es>
2026-06-03 07:52:37 +0100

    v7

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
-  // إعداد الـ webpack لتمرير الـ WebSockets وتفادي حصر الـ HMR
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      config.config = config.config || {};
-    }
-    return config;
-  },
+  // إسكات الخطأ وإعلام Next.js 16 بأنك باغي تخدم بـ Turbopack عادي
+  turbopack: {},
 };
 
 export default nextConfig;
