ddcbcf55f03446316a27bfa8028c98f939b2dde9
mahdi <eelmahdi2@alumno.uned.es>
2026-06-03 07:47:21 +0100

    v5

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  // إعداد الـ webpack لتمرير الـ WebSockets وتفادي حصر الـ HMR
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      config.config = config.config || {};
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
